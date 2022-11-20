a7c7671344a0981204f9a2d7c866e0f2826b00d7
ich_kirich <99396626+ich-kirich@users.noreply.github.com>
2022-11-20 16:23:50 +0300

    feat: add scores

--- src/scripts/quiz_music.js
+++ src/scripts/quiz_music.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", (e)=> {
     let nameMusicWarmUp = ["tennessee_whiskey", "minnie_the_moocher", "natural", "dance_knights", "mockingbird", "greensleeves"]
     let pictMusicWarmUp = ["blues", "jazz", "rock", "classical_music", "rap", "folk_music"]
     var answers = document.querySelectorAll('.answers__item')
+    let score = 5
 
     let num = Math.floor(Math.random() * (6 - 0)) + 0;
     music.src = './src/music/' + nameMusicWarmUp[num] + '.mp3'
@@ -13,16 +14,18 @@ document.addEventListener("DOMContentLoaded", (e)=> {
         answers.indexOf(answer)
         if(answers.indexOf(answer) === num) {
             answer.querySelector('.answers__li-btn').style.background = "#00D678"
-            document.querySelector('.panel-music__pict').classList.add(pictMusicWarmUp[num]);
+            document.querySelector('.panel-music__pict').classList.add(pictMusicWarmUp[num])
             str = pictMusicWarmUp[num].charAt(0).toUpperCase() + pictMusicWarmUp[num].slice(1)
-            var find = '_';
-            var re = new RegExp(find, 'g');
-            str = str.replace(re, ' ');
-            document.querySelector('.inform__name').innerText = str;
+            var find = '_'
+            var re = new RegExp(find, 'g')
+            str = str.replace(re, ' ')
+            document.querySelector('.inform__name').innerText = str
             document.querySelector('.next-level__to-next').style.backgroundColor = "rgba(77, 71, 244, 0.744)"
+            document.querySelector('.panel-top__score').innerText = `Score: ${score}`
         }
         else {
             answer.querySelector('.answers__li-btn').style.background = "#ff0000"
+            score--
         }
     }));
 });
